84f5933b700c9398456fe2da344b159ca1ea51cc
Steve Etherington <s_etherington@hotmail.com>
2013-10-08 20:45:10 +0100

    Use executorService for please shutdown behaviour.

--- src/main/java/cisco/telnet/TelnetServer.java
+++ src/main/java/cisco/telnet/TelnetServer.java
@@ -2,23 +2,27 @@ package cisco.telnet;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TelnetServer {
 
     private final int port;
+    private final ExecutorService executorService;
     private volatile boolean listening;
     private ServerSocket serverSocket;
 
     public TelnetServer(int port) {
         this.port = port;
         listening = true;
+        executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1);
     }
 
     public void start() {
         try {
             serverSocket = new ServerSocket(port);
             while (listening) {
-                new Thread(new TelnetTask(serverSocket.accept())).start();
+                executorService.submit(new TelnetTask(serverSocket.accept()));
             }
         } catch (IOException e) {
             throw new IllegalStateException(e);
@@ -29,6 +33,7 @@ public class TelnetServer {
         try {
             listening = false;
             serverSocket.close();
+            executorService.shutdownNow();
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
