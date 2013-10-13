88afaccc2c0d93ea67ccf017265cbbe2e8fa9c75
steve-e <githubpass1>
2013-10-13 18:06:28 +0100

    Use newfangled System.lineSeparator() for a listing that formats nicely on windows.

--- src/main/java/cisco/telnet/commands/LsCommand.java
+++ src/main/java/cisco/telnet/commands/LsCommand.java
@@ -2,12 +2,12 @@ package cisco.telnet.commands;
 
 import cisco.telnet.CommandResult;
 import org.apache.commons.lang.StringUtils;
-
 import java.io.File;
+import static java.lang.System.lineSeparator;
 
 public class LsCommand implements TelnetCommand {
     @Override
     public CommandResult executeFrom(File currentDirectory) {
-        return new CommandResult(StringUtils.join(currentDirectory.list(), "\n"), currentDirectory);
+        return new CommandResult(StringUtils.join(currentDirectory.list(), lineSeparator()), currentDirectory);
     }
 }
